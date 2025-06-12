9a980425996b86c889726081dc91b4258ebb9906
AmrutTest1 <mockimate.pro@gmail.com>
2025-06-12 13:39:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -156,4 +157,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
